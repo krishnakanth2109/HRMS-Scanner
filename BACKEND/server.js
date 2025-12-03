7cee47683e06dcfac2ed566115c925f9c922eec2
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2025-12-03 15:39:13 +0530

    .env updated

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,17 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
+
+// Load .env variables
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Connect DB
-mongoose.connect("mongodb+srv://ajayarahinfotech_db_user:Cms%40123@cms.lbqsmyv.mongodb.net/hrms?retryWrites=true&w=majority")
+// Connect DB from .env
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error(err));
+  .catch(err => console.error("DB Error:", err));
 
+// Routes
 app.use("/api/attendance", attendanceRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Server
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
